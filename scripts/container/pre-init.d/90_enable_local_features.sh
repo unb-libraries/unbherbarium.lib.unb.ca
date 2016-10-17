ee36ffb4a1e1ce81ef4c7b14f54152c58c0a7f01
Jacob Sanford <jsanford@unb.ca>
2016-10-17 09:17:53 -0300

    Move migration tools to composer dev

--- scripts/container/pre-init.d/90_enable_local_features.sh
+++ scripts/container/pre-init.d/90_enable_local_features.sh
@@ -7,7 +7,7 @@ if [ "$DEPLOY_ENV" = "local" ]; then
   DRUSH_COMMAND="drush --root=${DRUPAL_ROOT} --uri=default --yes"
   $DRUSH_COMMAND en migrate_plus migrate_source_csv migrate_tools migrate_upgrade
   $DRUSH_COMMAND cache-rebuild
-  $DRUSH_COMMAND en --yes migrate_tools
+
   $DRUSH_COMMAND en --yes unb_herbarium_migrate_csv
   $DRUSH_COMMAND migrate-status
   $DRUSH_COMMAND migrate-import --all
