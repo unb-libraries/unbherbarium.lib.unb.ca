e01b8ba22a21c0814335524f5a46e61c2f1e0c33
Jacob Sanford <jsanford@unb.ca>
2017-01-26 14:22:33 -0400

    Enable herbarium species feature on start

--- scripts/container/pre-init.d/90_enable_local_features.sh
+++ scripts/container/pre-init.d/90_enable_local_features.sh
@@ -8,7 +8,8 @@ if [ "$DEPLOY_ENV" = "local" ]; then
   $DRUSH_COMMAND en migrate_plus migrate_source_csv migrate_tools migrate_upgrade
   $DRUSH_COMMAND cache-rebuild
 
-  $DRUSH_COMMAND en --yes unb_herbarium_migrate_csv unb_herbarium
+  $DRUSH_COMMAND en --yes herbarium_species
+  $DRUSH_COMMAND en --yes unb_herbarium_migrate_csv
   $DRUSH_COMMAND migrate-status
   $DRUSH_COMMAND migrate-import --all
 fi
