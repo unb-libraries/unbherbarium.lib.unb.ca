82c251bb224ff46f94b6ba43dd3c64ba2f26c9fe
Jacob Sanford <jsanford@unb.ca>
2017-01-26 14:23:08 -0400

    Remove legacy config

--- scripts/container/pre-init.d/90_enable_local_features.sh
+++ scripts/container/pre-init.d/90_enable_local_features.sh
@@ -8,7 +8,9 @@ if [ "$DEPLOY_ENV" = "local" ]; then
   $DRUSH_COMMAND en migrate_plus migrate_source_csv migrate_tools migrate_upgrade
   $DRUSH_COMMAND cache-rebuild
 
-  $DRUSH_COMMAND en --yes herbarium_species
+  $DRUSH_COMMAND en --yes features herbarium_specimen
+  $DRUSH_COMMAND en --yes features_ui
+
   $DRUSH_COMMAND en --yes unb_herbarium_migrate_csv
   $DRUSH_COMMAND migrate-status
   $DRUSH_COMMAND migrate-import --all
