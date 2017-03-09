ea1c55322c7e132adf4fd12ced5c56c1faef0068
Jacob Sanford <jsanford@unb.ca>
2017-03-09 13:33:48 -0400

    HERB-24 Instantiate tile factory on image change

---
+++ custom/modules/herbarium/src/HerbariumImageTileFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\herbarium_specimen;
+
+use Drupal\node\Entity\Node;
+
+/**
+ * HerbariumImageTileFactory caption set object.
+ */
+class HerbariumImageTileFactory {
+
+  /**
+   * The Drupal File object to generate the DZI and tiles for.
+   *
+   * @var object
+   */
+  private $file;
+
+  /**
+   * Constructor.
+   *
+   * @param object $file
+   *   The Drupal File object to generate the DZI and tiles for.
+   */
+  protected function __construct($file) {
+    $this->file = $file;
+  }
+
+  /**
+   * Build DZI and custom map tiles for an image.
+   *
+   * @param object $file
+   *   The Drupal File object to generate the DZI and tiles for.
+   */
+  public static function BuildImageTiles($file) {
+    // Remove old image tile stuff
+    $obj = new static($file);
+  }
+
+}
