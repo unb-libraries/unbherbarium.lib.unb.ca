abb5cbc35dada84dd93565edac963fea701c9ee9
JeremyMcDermott <jeremy.mcdermott@unb.ca>
2017-03-10 11:31:33 -0400

    Custom UNB Herbarium 'Browse' exposed views filters

--- custom/modules/unb_herbarium/js/unb_herbarium.herbarium_specimen.js
+++ custom/modules/unb_herbarium/js/unb_herbarium.herbarium_specimen.js
@@ -3,4 +3,9 @@ jQuery(document).ready(function() {
         jQuery('.toggle-default-off').toggle(300);
         jQuery('.toggle-default-on').toggle(1);
     });
+
+    jQuery('.hide').hide();
+    jQuery('.toggle-filters-display').click(function() {
+        jQuery('form .views-exposed-form').slideToggle(300);
+    })
 });
