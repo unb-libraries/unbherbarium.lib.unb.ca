07da6cf6ae27f9311dd487254e95bc7df6b622a5
Jacob Sanford <jsanford@unb.ca>
2017-05-23 12:04:16 -0300

    Be verbose when enabling modules

--- scripts/container/pre-init.d/90_enable_local_features.sh
+++ scripts/container/pre-init.d/90_enable_local_features.sh
@@ -13,10 +13,16 @@ if [ "$DEPLOY_ENV" = "local" ]; then
 
   $DRUSH_COMMAND en --yes unb_herbarium_migrate_csv
   $DRUSH_COMMAND migrate-status
-  $DRUSH_COMMAND migrate-import --all
+  $DRUSH_COMMAND migrate-import herbarium_samples_csv
+
+  echo 'Rebuilding specimen taxonomy terms'
   $DRUSH_COMMAND eval '_herbarium_core_rebuild_specimen_taxonomy_terms();'
+
+  echo 'Enabling UNB herbarium theme'
   $DRUSH_COMMAND en unbherbarium_ca
   $DRUSH_COMMAND config-set system.theme default unbherbarium_ca
+
+  echo 'Enabling UNB herbarium module'
   $DRUSH_COMMAND en --yes unb_herbarium
   $DRUSH_COMMAND cache-rebuild
 fi
