a4d264660608508ee388c88f40f5a4fffffdfb5b
Jacob Sanford <jsanford@unb.ca>
2017-07-26 14:57:23 -0300

    HERB-10 clone bare to core local LFS repo

--- scripts/container/pre-init.d/30_deploy_git-lfs_repo.sh
+++ scripts/container/pre-init.d/30_deploy_git-lfs_repo.sh
@@ -12,7 +12,7 @@ if [ "$LTS_DEPLOY_KEY" != "" ] && [ "$LTS_DEPLOY_PATH" != "" ]; then
   # Clone LFS repo.
   mkdir -p ${LTS_DEPLOY_PATH}
   chown ${NGINX_RUN_USER}:${NGINX_RUN_GROUP} ${LTS_DEPLOY_PATH}
-  su - ${NGINX_RUN_USER} -s /bin/bash -c "GIT_SSH_COMMAND=\"ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${NGINX_USER_HOME}/.ssh/id_rsa \" /usr/bin/git clone ${LTS_DEPLOY_REPO} ${LTS_DEPLOY_PATH}"
+  su - ${NGINX_RUN_USER} -s /bin/bash -c "GIT_SSH_COMMAND=\"ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${NGINX_USER_HOME}/.ssh/id_rsa \" /usr/bin/git clone --bare ${LTS_DEPLOY_REPO} ${LTS_DEPLOY_PATH}"
   cd ${LTS_DEPLOY_PATH}
   echo -e "[lfs]\n    url = \"http://${LTS_LFS_SERVER_USER}:${LTS_LFS_SERVER_PASS}@${LTS_LFS_SERVER_HOST}:${LTS_LFS_SERVER_PORT}/\"\n" > .lfsconfig
   chown ${NGINX_RUN_USER}:${NGINX_RUN_GROUP} .lfsconfig
