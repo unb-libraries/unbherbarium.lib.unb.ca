219fc5ef2ee491f8136d2dd8f1b0fbfc9e666136
Jacob Sanford <jsanford@unb.ca>
2017-11-01 15:22:24 -0300

    Add bulk image regeneratorororior

---
+++ scripts/container/bulkImageUpdate.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+cd /lts-archive/
+GIT_FILE_LIST=$(git ls-tree -r master --name-only)
+FILE_EXTENSION='tif'
+DZI_PATH='/app/html/sites/default/files/dzi'
+NUM_TO_OUTPUT='2'
+
+OUTPUT_COUNT=0
+echo "$GIT_FILE_LIST" | while read -r a; do
+
+  if [[ "${a/$FILE_EXTENSION}" != "$a" ]]; then
+    NODE_ID=$(echo $a | cut -f 1 -d '.')
+    if [[ ! -f "$DZI_PATH/$NODE_ID.dzi" ]]; then
+      echo "$NODE_ID"
+      su -l -s /bin/sh nginx -c "drush --root=/app/html -u 1 regenerate-surrogates $NODE_ID" &
+      sleep 2
+      OUTPUT_COUNT=$((OUTPUT_COUNT+1))
+    fi
+  fi
+
+  if [[ "$OUTPUT_COUNT" -gt "$NUM_TO_OUTPUT" ]]; then
+    exit 0
+  fi
+done
