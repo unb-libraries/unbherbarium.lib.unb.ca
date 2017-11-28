4f05a1c9d2182d60d426fa814d666653e6d6742b
JeremyMcDermott <jeremy.mcdermott@unb.ca>
2017-11-28 11:04:14 -0400

    Use double quotes, add back button state fix

--- custom/modules/herbarium/modules/herbarium_specimen/js/herbarium_specimen.views_form.js
+++ custom/modules/herbarium/modules/herbarium_specimen/js/herbarium_specimen.views_form.js
@@ -1,5 +1,12 @@
 jQuery(document).ready(function () {
     // Workaround: change Exposed filters type from "text' to 'date' for mindate & maxdate fields.
-    jQuery('#edit-mindate').prop('type', 'date');
-    jQuery('#edit-maxdate').prop('type', 'date');
+    jQuery("#edit-mindate").prop("type", "date");
+    jQuery("#edit-maxdate").prop("type", "date");
+
+    // Browser back button checkbox state fix.
+    if (jQuery("#toggle-widget input").is(":checked")) {
+        jQuery(".toggle-default-off").show();
+    } else {
+        jQuery(".toggle-default-off").hide();
+    }
 });
