19bdff25b5a2ab25961c43dd70703cf7f37c805d
Jacob Sanford <jsanford@unb.ca>
2018-02-12 13:17:20 -0400

    HERB-12 Add exporter plugin for search results CSV downloading

---
+++ custom/modules/herbarium/modules/herbarium_specimen_csv_export/src/Plugin/views/area/AddCSVExportButton.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\herbarium_specimen_csv_export\Plugin\views\area;
+
+use Drupal\Core\Url;
+use Drupal\views\Plugin\views\area\AreaPluginBase;
+
+/**
+ * Views area collection add new button handler.
+ *
+ * @ingroup views_area_handlers
+ *
+ * @ViewsArea("herbarium_specimen_csv_export_button")
+ */
+class AddCSVExportButton extends AreaPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+    // Set the default to TRUE so it shows on empty pages by default.
+    $options['empty']['default'] = FALSE;
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render($empty = FALSE) {
+    if (!$empty || !empty($this->options['empty'])) {
+      $view = $this->view;
+
+      $csv_nodes = [];
+      foreach ($view->result as $row) {
+        $csv_nodes[] = $row->_entity->id();
+      }
+
+      $button = [
+        '#type' => 'link',
+        '#id' => 'csv-bulk-export-download',
+        '#title' => $this->t('Download Results as CSV'),
+        '#url' => Url::fromRoute(
+          'herbarium_specimen_csv_export.bulk_download',
+          [
+            'node_ids' => implode('|', $csv_nodes),
+          ]
+        ),
+        '#prefix' => '<div>',
+        '#suffix' => '</div>',
+        '#attached' => [
+          'library' => [
+            'herbarium_specimen_csv_export/csv_export_button',
+          ],
+        ],
+      ];
+
+      return $button;
+    }
+  }
+
+}
