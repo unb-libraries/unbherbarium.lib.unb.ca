fbc623fea38c35b6c9b14fb434295631d1640dc3
jsanford <jsanford@unb.ca>
2019-07-09 12:41:54 -0300

    Changes related to upstream settings management

--- build/settings/base.settings.php
+++ build/settings/base.settings.php
@@ -5,19 +5,10 @@
  * Include global settings overrides here.
  */
 
-// Environment based includes.
-if (isset($_SERVER['APPLICATION_ENV'])) {
-  $environment = strtolower($_SERVER['APPLICATION_ENV']);
-  $environment_include = dirname(__FILE__) . "/settings.$environment.inc";
-  if (file_exists($environment_include)) {
-    require_once $environment_include;
-  }
-}
-
 // Redis.
 $settings['cache_prefix']['default'] = 'unbherb_';
 $conf['chq_redis_cache_enabled'] = TRUE;
 require_once dirname(__FILE__) . "/settings.redis.inc";
 
 // Set the private filesystem path.
-$settings['file_private_path']  = '/app/private_filesystem';
+$settings['file_private_path'] = '/app/private_filesystem';
