66f4df193af6d034c89576c3b571dafb7b7382e4
Jacob Sanford <jsanford@unb.ca>
2021-11-24 09:51:30 -0400

    HERB-162 Inital base 9.x conversion

--- build/scripts/container/pre-init.d/80_set_private_filesystem.sh
+++ build/scripts/container/pre-init.d/80_set_private_filesystem.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 mkdir -p ${DRUPAL_PRIVATE_FILE_PATH} && chown ${NGINX_RUN_USER}:${NGINX_RUN_GROUP} ${DRUPAL_PRIVATE_FILE_PATH}
 DRUSH_COMMAND="drush --root=${DRUPAL_ROOT} --uri=default --yes"
-$DRUSH_COMMAND eval 'use Drupal\Component\PhpStorage\FileStorage; print FileStorage::htaccessLines(FALSE)' > ${DRUPAL_PRIVATE_FILE_PATH}/.htaccess
+$DRUSH_COMMAND eval 'use Drupal\Component\FileSecurity\FileSecurity; print FileSecurity::htaccessLines(FALSE)' > ${DRUPAL_PRIVATE_FILE_PATH}/.htaccess
 chown ${NGINX_RUN_USER}:${NGINX_RUN_GROUP} ${DRUPAL_PRIVATE_FILE_PATH}/.htaccess
