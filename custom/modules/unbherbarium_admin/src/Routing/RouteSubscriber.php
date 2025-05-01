d2e197a0688078d7a8efc939132bc0399ed6b4ce
Camilo Villamizar <cvillami@unb.ca>
2025-05-01 12:07:50 -0300

    HERB-188 Add/config module unbherbarium_admin +toolbar

---
+++ custom/modules/unbherbarium_admin/src/Routing/RouteSubscriber.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\unbherbarium_admin\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events and restrict access to routes.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterRoutes(RouteCollection $collection) {
+    // Restrict user admin routes.
+    $deny_routes = [
+      'user.pass',
+      'user.register',
+      'user.reset',
+      'entity.user.edit_form',
+      'contact.site_page',
+    ];
+
+    // Deny access to non-admins.
+    foreach ($deny_routes as $deny_route) {
+      if ($route = $collection->get($deny_route)) {
+        $route->setRequirement('_permission', 'administer_users');
+      }
+    }
+  }
+
+}
